0c5594270c0c0e8994d80a5fc3f19a982640e4e8
dev-ajay-git <er.ajay88@gmail.com>
2025-03-02 23:20:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-"main": "index.js",
-"scripts": {
-    "start": "node index.js"
-}
+// index.js - A simple Node.js server
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, Jenkins!");
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/");
+});
